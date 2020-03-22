7536c8a84d1a1c9d9698e3d90637df76aa29e733
Ivan Shukalo <shukalo.96@gmail.com>
2020-03-22 21:50:35 +0300

    feah: add function calculator

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-function eval() {
-    // Do not use eval!!!
-    return;
-}
-
 function expressionCalculator(expr) {
-    // write your solution here
+    
+    while (/\(.*\)/.test(expr)) {
+        let openingBracket = expr.lastIndexOf('(');
+        let closingBracket = expr.indexOf(')', openingBracket + 1);
+        let innerExpression = expr.slice(openingBracket + 1, closingBracket);
+        expr = expr.replace(`(${innerExpression})`, expressionCalculator(innerExpression));
+    }
+    
+    if (/[()]/.test(expr)) throw new Error('ExpressionError: Brackets must be paired');
+    
+    for (let mark of ['/', '*', '-', '+']) {
+        
+        let signRegEx = new RegExp(`\\d+\\s*\\${mark}\\s*[+\\-]?\\d+`);
+        
+        while(signRegEx.test(expr)) {
+            let [exprPart, numOne, numTwo] = expr.match(`([+\\-]?[\\d.e\\-]+)\\s*\\${mark}\\s*([+\\-]?[\\d.e\\-]+)`);
+            
+            if (numTwo === '0' && mark === '/') throw new Error('TypeError: Division by zero.');
+            
+            if (mark === '+') expr = expr.replace(exprPart, +numOne + +numTwo);
+            else if (mark === '-') expr = expr.replace(exprPart, numOne - numTwo);
+            else if (mark === '*') expr = expr.replace(exprPart, numOne * numTwo);
+            else if (mark === '/') expr = expr.replace(exprPart,  numOne / numTwo);
+        }
+    }
+    
+    return parseFloat(expr);
+    
 }
 
 module.exports = {
     expressionCalculator
-}
+};
